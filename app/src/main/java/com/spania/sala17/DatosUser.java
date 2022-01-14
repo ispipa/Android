cb515c76ab0fe33ac1f7f505316382116ce75a95
HP196\CES <informatica@escuelaces.com>
2022-01-14 09:18:38 +0100

    Creada zona datos del usuario y botones de secciones

---
+++ app/src/main/java/com/spania/sala17/DatosUser.java
@@ -0,0 +1,16 @@
+package com.spania.sala17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatosUser extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_user);
+    }
+}
