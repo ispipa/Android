1306d0089b8506190b1bdfa9ec8aa755f7c79694
Alex <rgalexmv@gmail.com>
2022-01-18 19:56:51 +0100

    Implementacion de Tinder copas

--- app/src/main/java/com/spania/sala17/Tinder.java
+++ app/src/main/java/com/spania/sala17/Tinder.java
@@ -1,128 +1,77 @@
 package com.spania.sala17;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.daprlabs.cardstack.SwipeDeck;
+import com.spania.sala17.CourseModal;
+import com.spania.sala17.DeckAdapter;
+import com.spania.sala17.R;
 
 import java.util.ArrayList;
 
 public class Tinder extends AppCompatActivity {
-ArrayList<String>s;
-ArrayList<String>nombre = new ArrayList<String>();
-ArrayAdapter arrayAdapter;
+    // on below line we are creating variable
+    // for our array list and swipe deck.
+    private SwipeDeck cardStack;
+    private ArrayList<CourseModal> courseModalArrayList;
 
-boolean Female = false;
-int n=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tinder);
 
+        // on below line we are initializing our array list and swipe deck.
+        courseModalArrayList = new ArrayList<>();
+        cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
 
+        // on below line we are adding data to our array list.
+        courseModalArrayList.add(new CourseModal("C++", "30 days", "20 Tracks", "C++ Self Paced Course", R.drawable.cub));
+        courseModalArrayList.add(new CourseModal("Java", "30 days", "20 Tracks", "Java Self Paced Course", R.drawable.cub));
+        courseModalArrayList.add(new CourseModal("Python", "30 days", "20 Tracks", "Python Self Paced Course", R.drawable.cub));
+        courseModalArrayList.add(new CourseModal("DSA", "30 days", "20 Tracks", "DSA Self Paced Course", R.drawable.cub));
+        courseModalArrayList.add(new CourseModal("PHP", "30 days", "20 Tracks", "PHP Self Paced Course", R.drawable.cub));
 
-        s=new ArrayList<String>();
-        if(Female == false) {
-            s.add("one");
-            s.add("dos");
-            s.add("tres");
-            s.add("cuatro");
-            s.add("cinco");
-            s.add("seis");
-            s.add("siete");
-            s.add("one");
-            s.add("dos");
-            s.add("tres");
-            s.add("cuatro");
-            s.add("cinco");
-            s.add("seis");
-            s.add("siete");
-        }else{
-            s.add("ola");
-            s.add("ola");
-            s.add("tres");
-            s.add("cuatro");
-            s.add("cinco");
-            s.add("seis");
-            s.add("siete");
-        }
-        SwipeFlingAdapterView swipeFlingAdapterView=(SwipeFlingAdapterView) findViewById(R.id.card);
+        // on below line we are creating a variable for our adapter class and passing array list to it.
+        final DeckAdapter adapter = new DeckAdapter(courseModalArrayList, this);
 
+        // on below line we are setting adapter to our card stack.
+        cardStack.setAdapter(adapter);
 
-
-        arrayAdapter = new ArrayAdapter<String>(this,R.layout.detail,R.id.text,s);
-        swipeFlingAdapterView.setAdapter(arrayAdapter);
-
-        swipeFlingAdapterView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+        // on below line we are setting event callback to our card stack.
+        cardStack.setEventCallback(new SwipeDeck.SwipeEventCallback() {
             @Override
-            public void removeFirstObjectInAdapter() {
-                s.remove(0);
-                arrayAdapter.notifyDataSetChanged();
+            public void cardSwipedLeft(int position) {
+                // on card swipe left we are displaying a toast message.
+                Toast.makeText(Tinder.this, "Card Swiped Left", Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onLeftCardExit(Object o) {
-                Toast.makeText(Tinder.this,"dislike",Toast.LENGTH_SHORT).show();
+            public void cardSwipedRight(int position) {
+                // on card swiped to right we are displaying a toast message.
+                Toast.makeText(Tinder.this, "Card Swiped Right", Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onRightCardExit(Object o) {
-                Toast.makeText(Tinder.this,"like",Toast.LENGTH_SHORT).show();
-
+            public void cardsDepleted() {
+                // this method is called when no card is present
+                Toast.makeText(Tinder.this, "No more courses present", Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onAdapterAboutToEmpty(int i) {
-
+            public void cardActionDown() {
+                // this method is called when card is swiped down.
+                Log.i("TAG", "CARDS MOVED DOWN");
             }
 
             @Override
-            public void onScroll(float v) {
-
+            public void cardActionUp() {
+                // this method is called when card is moved up.
+                Log.i("TAG", "CARDS MOVED UP");
             }
         });
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.tinder:
-                Toast.makeText(Tinder.this,"Entrando a tinder ",Toast.LENGTH_SHORT).show();
-                Intent t = new Intent(this, Tinder.class);
-                startActivity(t);
-                break;
-            case R.id.copas:
-                Intent i = new Intent(this, Copas.class);
-                Toast.makeText(Tinder.this, "Entrando a copas", Toast.LENGTH_SHORT).show();
-                startActivity(i);
-                break;
-            case R.id.eventos:
-                Intent e = new Intent(this,Eventos.class);
-                startActivity(e);
-                Toast.makeText(Tinder.this,"Entrando a eventos", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.music:
-                Intent m = new Intent(this,Music.class);
-                startActivity(m);
-                Toast.makeText(Tinder.this, "Entrando a Spotify",Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                Toast.makeText(Tinder.this,"Reinicie la aplicacion", Toast.LENGTH_LONG).show();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
